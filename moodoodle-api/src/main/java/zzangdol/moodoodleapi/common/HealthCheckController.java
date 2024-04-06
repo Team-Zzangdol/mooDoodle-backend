58de018281e8d39507e55bf88c74e3d39e82a9c9
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-04-06 22:26:40 +0900

    :sparkles: Feature: FeignClient를 이용한 모델 API 연동 (#16)

--- moodoodle-api/src/main/java/zzangdol/moodoodleapi/common/HealthCheckController.java
+++ moodoodle-api/src/main/java/zzangdol/moodoodleapi/common/HealthCheckController.java
@@ -2,9 +2,7 @@ package zzangdol.moodoodleapi.common;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import zzangdol.moodoodlecommon.exception.GeneralException;
 import zzangdol.moodoodlecommon.response.ApiResponse;
-import zzangdol.moodoodlecommon.response.status.ErrorStatus;
 
 @RestController
 public class HealthCheckController {
---
+++ moodoodle-infrastructure/src/main/java/zzangdol/feign/client/Text2ImageModelClient.java
@@ -0,0 +1,15 @@
+package zzangdol.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import zzangdol.dto.ContentDto;
+import zzangdol.dto.MessageDto;
+
+@FeignClient(name = "text2ImageModelClient", url = "${outer-api.text-2-image.url}")
+public interface Text2ImageModelClient {
+
+    @GetMapping
+    MessageDto getMessage(@RequestBody ContentDto request);
+
+}
