a78fce7e6a1149dff631b479f7eac75c9115325b
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-04-12 19:28:46 +0900

    :sparkles: Feature: 이메일 인증 성공 후 인증번호 Redis에서 삭제 처리 (#23)

--- moodoodle-common/src/main/java/zzangdol/moodoodlecommon/response/status/ErrorStatus.java
+++ moodoodle-common/src/main/java/zzangdol/moodoodlecommon/response/status/ErrorStatus.java
@@ -11,7 +11,12 @@ public enum ErrorStatus implements BaseStatus {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5000, "서버 에러, 담당자에게 문의 바랍니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, 4000, "잘못된 요청입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 4001, "로그인이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, 4002, "금지된 요청입니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, 4002, "금지된 요청입니다."),
+
+    // Auth (4050 ~ 4051)
+    VERIFICATION_CODE_EXPIRED(HttpStatus.GONE, 4050, "인증 코드가 만료되었습니다."),
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, 4051, "잘못된 인증 코드입니다.");
+
 
     private final HttpStatus httpStatus;
     private final int code;
