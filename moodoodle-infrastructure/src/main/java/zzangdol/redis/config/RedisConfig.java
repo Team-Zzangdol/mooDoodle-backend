d8c2dce704cffdbc6682b0f94e4948dacff1afc6
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-04-13 01:26:18 +0900

    🐞 BugFix: Redis 설정 시 CONFIG 명령어 실행 문제 해결 (#25)

--- moodoodle-infrastructure/src/main/java/zzangdol/redis/config/RedisConfig.java
+++ moodoodle-infrastructure/src/main/java/zzangdol/redis/config/RedisConfig.java
@@ -1,18 +1,21 @@
 package zzangdol.redis.config;
 
-import static org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import zzangdol.InfrastructurePackageLocation;
 
-@EnableRedisRepositories(basePackageClasses = InfrastructurePackageLocation.class, enableKeyspaceEvents = ON_STARTUP)
+@EnableRedisRepositories(
+        basePackageClasses = InfrastructurePackageLocation.class,
+        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP,
+        keyspaceNotificationsConfigParameter = ""
+)
 @Configuration
 public class RedisConfig {
 
