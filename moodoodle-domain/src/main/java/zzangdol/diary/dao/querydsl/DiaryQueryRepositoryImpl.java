d9d6e449fe166429e2da9adb6d23b3702ff074a3
ahnsugyeong <ahnsugyeong@gmail.com>
2024-05-06 16:07:32 +0900

    🐞 BugFix: 주간 일기 작성 쿼리 타입 LocalDate로 변경

--- moodoodle-domain/src/main/java/zzangdol/diary/dao/querydsl/DiaryQueryRepositoryImpl.java
+++ moodoodle-domain/src/main/java/zzangdol/diary/dao/querydsl/DiaryQueryRepositoryImpl.java
@@ -5,7 +5,6 @@ import static zzangdol.diary.domain.QDiary.diary;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -29,20 +28,15 @@ public class DiaryQueryRepositoryImpl implements DiaryQueryRepository {
 
     @Override
     public List<Diary> findDiariesByUserAndYearAndMonthAndWeek(Long userId, int year, int month, int week) {
-        QDiary diary = QDiary.diary;
-
         LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
         LocalDate firstSunday = firstDayOfMonth.with(DayOfWeek.SUNDAY);
         LocalDate weekStartDate = firstSunday.plusWeeks(week - 1);
         LocalDate weekEndDate = weekStartDate.plusWeeks(1);
 
-        LocalDateTime startDateTime = weekStartDate.atStartOfDay();
-        LocalDateTime endDateTime = weekEndDate.atStartOfDay();
-
         return queryFactory
                 .selectFrom(diary)
                 .where(diary.user.id.eq(userId)
-                        .and(diary.date.between(startDateTime, endDateTime)))
+                        .and(diary.date.between(weekStartDate, weekEndDate)))
                 .fetch();
     }
 
