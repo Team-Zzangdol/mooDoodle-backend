dfa271426aba8ae9bb543d9673b5a4192828cb98
ahnsugyeong <ahnsugyeong@gmail.com>
2024-05-28 21:35:43 +0900

    :sparkles: Feature: 회원가입 시 기본 카테고리 생성

--- moodoodle-common/src/main/java/zzangdol/constant/Constants.java
+++ moodoodle-common/src/main/java/zzangdol/constant/Constants.java
@@ -3,5 +3,6 @@ package zzangdol.constant;
 public class Constants {
 
     public static final String DEFAULT_IMAGE_URL = "https://moodoodle-dev.s3.ap-northeast-2.amazonaws.com/default_category_image.png";
+    public static final String DEFAULT_CATEGORY_NAME = "모든 카테고리";
 
 }
