ba02a8f790d9f15c4cfc9d9558b9927ffc51e016
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-06-07 16:31:06 +0900

    :sparkles: Feature: 감정 분석 모델 연동 (#110)

--- moodoodle-domain/src/main/java/zzangdol/emotion/dao/EmotionRepository.java
+++ moodoodle-domain/src/main/java/zzangdol/emotion/dao/EmotionRepository.java
@@ -1,7 +1,11 @@
 package zzangdol.emotion.dao;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zzangdol.emotion.domain.Emotion;
 
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
+
+    Optional<Emotion> findByName(String name);
+
 }
